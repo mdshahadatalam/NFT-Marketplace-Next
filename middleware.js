098d339ae4625ff04a6bc4cb4b69fe590993f906
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-06-04 21:35:07 -0700

    edit

--- middleware.js
+++ middleware.js
@@ -1,2 +1,2 @@
-export {default} from 'next-auth/middleware';
-export const config = { matcher:['/Wallet']};
+// export {default} from 'next-auth/middleware';
+// export const config = { matcher:['/Wallet']};
